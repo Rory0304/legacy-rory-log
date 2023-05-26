ee4fc04025905bbe15c060bc70ddcb859864997c
Eunsoo Sa <eunsoo@elicer.com>
2023-05-26 18:13:27 +0900

    feat(trigger): add revalidate page util

---
+++ src/app/api/trigger/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+import { revalidatePath } from "next/cache";
+
+const ACCESS_TOKEN =
+  process.env.NEXT_PUBLIC_CONTENTFUL_CONTENT_DELIVERY_API_ACCESS_TOKEN ?? "";
+
+export const POST = async (req: NextRequest) => {
+  if (
+    req.method !== "POST" ||
+    req.headers.get("x-rorydev-content-header") !== ACCESS_TOKEN
+  ) {
+    console.log(req.headers.get("x--contentful-topic"), req.method);
+    return NextResponse.json({ message: "Invalid request" }, { status: 400 });
+  }
+
+  const json = await req.json();
+  const slug = json.fields.slug["en-US"] as string;
+
+  if (!slug) {
+    return NextResponse.json({ message: "Slug not found" }, { status: 400 });
+  }
+
+  Promise.all([revalidatePath(slug)]).catch((err) => {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Fail to revalidate" },
+      { status: 400 }
+    );
+  });
+
+  return NextResponse.json({ message: "Success" }, { status: 200 });
+};
