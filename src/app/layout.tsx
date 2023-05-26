c32754602f1ab11fc0197984d0ac0dcd6eef35eb
Eunsoo Sa <eunsoo@elicer.com>
2023-05-26 18:05:00 +0900

    feat(Layout): remove client in root layout and add provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,15 @@
-"use client";
-
 import {
   GlobalMain,
   GlobalHeader,
   GlobalFooter,
 } from "src/components/blocks/Global";
-import { CssBaseline } from "@mui/material";
-import emotionReset from "emotion-reset";
-import { Global } from "@emotion/react";
-import { resetStyles } from "src/constants/styles/resetStyles";
-
-import MuiThemeProvider from "@mui/system/ThemeProvider";
-import { createTheme } from "@mui/material/styles";
-import { IntlProvider } from "react-intl";
-import { ApolloProvider } from "@apollo/client";
-
-import apolloClient from "src/graphql/client";
+import { AppProvider, ThemeProvider } from "src/contexts";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // Create a theme instance.
-  const theme = createTheme({
-    palette: {
-      mode: "light",
-    },
-  });
-
   return (
     <html lang="ko">
       <head>
@@ -40,19 +21,16 @@ export default function RootLayout({
         />
         <link rel="icon" href={`/favicon.ico`} />
       </head>
-      <ApolloProvider client={apolloClient}>
-        <IntlProvider locale="ko">
-          <MuiThemeProvider theme={theme}>
-            <CssBaseline />
-            <Global styles={[emotionReset, resetStyles]} />
-            <body>
-              <GlobalHeader />
-              <GlobalMain>{children}</GlobalMain>
-              <GlobalFooter />
-            </body>
-          </MuiThemeProvider>
-        </IntlProvider>
-      </ApolloProvider>
+
+      <AppProvider>
+        <ThemeProvider>
+          <body>
+            <GlobalHeader />
+            <GlobalMain>{children}</GlobalMain>
+            <GlobalFooter />
+          </body>
+        </ThemeProvider>
+      </AppProvider>
     </html>
   );
 }
