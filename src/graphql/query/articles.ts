a95443378fc46013bdcb4a2f721dc2af7a424d46
Rory <mitty0304@naver.com>
2023-05-29 18:04:57 +0900

    feat(articles): add query for getting article slugs

--- src/graphql/query/articles.ts
+++ src/graphql/query/articles.ts
@@ -1,15 +1,24 @@
 import { gql } from "@apollo/client";
 import { FRAGMENT_ARTICLE } from "../fragment";
 
-
 export const GET_ARTICLES = gql`
-    query GetArticles {
-        articleCollection {
-            items {
-                ...ArticleFields
-            }
-        }
+  query GetArticles {
+    articleCollection(order: date_DESC) {
+      items {
+        ...ArticleFields
+      }
     }
+  }
 
-    ${FRAGMENT_ARTICLE}
+  ${FRAGMENT_ARTICLE}
+`;
+
+export const GET_ARTICLE_SLUGS = gql`
+  query GetArticleSlugs {
+    articleCollection {
+      items {
+        slug
+      }
+    }
+  }
 `;
