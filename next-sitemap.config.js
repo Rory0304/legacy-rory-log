83c4b91afcc55b6c7fb183020a4307bf33c478a0
Rory <mitty0304@naver.com>
2023-06-21 17:01:23 +0900

    chore: fix page url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_RORY_DEV || "https://rory-dev.com/",
+  siteUrl: process.env.NEXT_PUBLIC_RORY_DEV || "https://rory0304.github.io",
   generateRobotsTxt: true,
   exclude: ["/404"],
   sitemapBaseFileName: "sitemap/sitemap",
---
+++ src/constants/config.ts
@@ -0,0 +1,14 @@
+export const SITE_CONFIG = {
+  url: "https://rory0304.github.io",
+  title: "rory dev log",
+  description: "Rory's dev log",
+  copyright: "© Rory. All rights reserved",
+  since: 2023,
+  googleAnalyticsId: "",
+  author: {
+    name: "Rory",
+    contacts: {
+      email: "mitty0304@naver.com",
+    },
+  },
+};
