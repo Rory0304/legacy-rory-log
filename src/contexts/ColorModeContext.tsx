22e9d2a676fe4164cf3f7c274b3bba2bac4f5519
Rory <mitty0304@naver.com>
2023-06-23 02:24:00 +0900

    feat(context): add color mode global context

---
+++ src/contexts/ColorModeContext.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ColorModeContext = React.createContext({
+  toggleColorMode: () => {},
+});
+
+export default ColorModeContext;
--- src/contexts/ThemeProvider.tsx
+++ src/contexts/ThemeProvider.tsx
@@ -6,20 +6,56 @@ import emotionReset from "emotion-reset";
 import { Global } from "@emotion/react";
 import { resetStyles } from "src/constants/styles/resetStyles";
 
+import ColorModeContext from "src/contexts/ColorModeContext";
 import MuiThemeProvider from "@mui/system/ThemeProvider";
-import { theme } from "src/styles/theme";
+import createTheme from "@mui/material/styles/createTheme";
+import { palette } from "src/styles/palette";
+
+//
+//
+//
+type ColorModeType = "light" | "dark";
 
 interface ThemeProviderProps {
   children: React.ReactNode;
 }
 
+//
+//
+//
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [mode, setMode] = React.useState<ColorModeType>("dark");
+
+  React.useEffect(() => {
+    if (window) {
+      const systemPrefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setMode(systemPrefersDark ? "dark" : "light");
+    }
+  }, []);
+
+  const toggleColorMode = React.useCallback(() => {
+    setMode((prevMode: ColorModeType) =>
+      prevMode === "light" ? "dark" : "light"
+    );
+  }, []);
+
+  const theme = createTheme({
+    palette: {
+      mode,
+      ...palette,
+    },
+  });
+
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <Global styles={[emotionReset, resetStyles]} />
-      {children}
-    </MuiThemeProvider>
+    <ColorModeContext.Provider value={{ toggleColorMode }}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Global styles={[emotionReset, resetStyles]} />
+        {children}
+      </MuiThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
