67ed3c1ac3e22892039be106af420b4e88e7cd80
Rory <mitty0304@naver.com>
2023-06-23 02:22:08 +0900

    style: change to palette variable

---
+++ src/styles/palette.ts
@@ -0,0 +1,14 @@
+export const palette = {
+  primary: {
+    main: "#FF7F50",
+    light: "#FF7F50",
+    dark: "#FF7F50",
+    contrastText: "#fff",
+  },
+  neutral: {
+    main: "#64748B",
+    light: "#64748B",
+    dark: "#64748B",
+    contrastText: "#fff",
+  },
+};
