4849b14f2b9f3a3aaf5aedf40ceee8905b8cee65
Rory <mitty0304@naver.com>
2023-07-04 15:26:27 +0900

    refactor(useColorMode): fix some logics

--- src/hooks/useColorMode.ts
+++ src/hooks/useColorMode.ts
@@ -10,19 +10,10 @@ const useColorMode = () => {
   const isDarkMode = theme.palette.mode === "dark";
 
   React.useEffect(() => {
-    const prevMode = theme.palette.mode === "dark" ? "light" : "dark";
     const currentMode = theme.palette.mode;
 
     // Set dataset on body to allow for global styling based on light/dark mode
     document.body.dataset.theme = currentMode;
-
-    // Change highligh stylesheet based on light/dark mode
-    document
-      .querySelector(`link[title="${currentMode}"]`)
-      ?.removeAttribute("disabled");
-    document
-      .querySelector(`link[title="${prevMode}"]`)
-      ?.setAttribute("disabled", "disabled");
   }, [theme]);
 
   return { isDarkMode, toggleColorMode };
