0987e7b7bd40dda6ea999cf506be6f8241d829b9
Rory <mitty0304@naver.com>
2023-08-09 16:39:08 +0900

    feat(PageSeo): add page seo

--- src/components/pages/PageSeo/PageSeo.tsx
+++ src/components/pages/PageSeo/PageSeo.tsx
@@ -8,10 +8,16 @@ import { usePathname } from "next/navigation";
 interface PageSeoProps {
   title: string;
   description: string;
+  imageUrl?: string;
   canonical?: string;
 }
 
-const PageSeo: React.FC<PageSeoProps> = ({ title, description, canonical }) => {
+const PageSeo: React.FC<PageSeoProps> = ({
+  title,
+  description,
+  imageUrl,
+  canonical,
+}) => {
   const pathname = usePathname();
 
   return (
@@ -19,6 +25,13 @@ const PageSeo: React.FC<PageSeoProps> = ({ title, description, canonical }) => {
       title={title ?? undefined}
       description={description ?? undefined}
       canonical={canonical ?? pathname ?? undefined}
+      openGraph={{
+        type: "website",
+        title: title ?? undefined,
+        description: description ?? undefined,
+        url: pathname ?? undefined,
+        images: imageUrl ? [{ url: imageUrl }] : undefined,
+      }}
     />
   );
 };
