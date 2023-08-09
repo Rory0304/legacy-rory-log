dc9fa0bef656285a1cbbc9e13ea274fbd11fccf1
Rory <mitty0304@naver.com>
2023-08-09 16:37:12 +0900

    fix(next.config.js): remove output export option

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     // ref: https://nextjs.org/docs/messages/export-image-api
     unoptimized: true,
