7c6e5229e658bf20ff4a605669a6efd78b1e7902
Rory <mitty0304@naver.com>
2023-08-21 02:06:05 +0900

    feat(next.config.js): add transpile package and moduleize imports

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  transpilePackages: ["@mui/material", "@mui/icons-material"],
+  modularizeImports: {
+    "@mui/material/?(((\\w*)?/?)*)": {
+      transform: "@mui/material/{{ matches.[1] }}/{{member}}",
+    },
+    "@mui/icons-material/?(((\\w*)?/?)*)": {
+      transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
+    },
+  },
   images: {
     // ref: https://nextjs.org/docs/messages/export-image-api
     unoptimized: true,
