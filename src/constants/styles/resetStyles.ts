38991b1218f1433775b7d55f93734ec40521098f
Rory <mitty0304@naver.com>
2023-08-21 02:15:15 +0900

    style(resetStyles): add style reset codes

--- src/constants/styles/resetStyles.ts
+++ src/constants/styles/resetStyles.ts
@@ -1,6 +1,10 @@
 import { css } from "@emotion/react";
 
 export const resetStyles = css`
+  :root {
+    --sticky-header-height: 68.5px;
+  }
+
   body {
     transition: background-color 0.3s ease, color 0.3s ease;
   }
@@ -8,5 +12,30 @@ export const resetStyles = css`
   a {
     color: inherit;
     text-decoration: none;
+  }
+
+  body > header {
+    backdrop-filter: saturate(180%) blur(5px);
+    transition: linear 0.2s;
+  }
+
+  body[data-theme="dark"] {
+    background-color: rgb(23 23 23/1);
+    color: #fff;
+
+    > header {
+      background-color: rgba(0, 0, 0, 0.5);
+      box-shadow: inset 0 -1px 0 0 hsla(0, 0%, 100%, 0.1);
+    }
+  }
+
+  body[data-theme="light"] {
+    background-color: #fff;
+    color: #000;
+
+    > header {
+      background-color: hsla(0, 0%, 100%, 0.8);
+      box-shadow: inset 0 -1px 0 0 #eaeaea;
+    }
   }
 `;
