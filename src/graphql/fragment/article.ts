ae706f148682cee4919cf6e17ae9c175ca3f3051
Rory <mitty0304@naver.com>
2023-08-22 15:51:18 +0900

    feat(article): add thumbnail url option for optimization

--- src/graphql/fragment/article.ts
+++ src/graphql/fragment/article.ts
@@ -1,17 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const FRAGMENT_ARTICLE = gql`
-    fragment ArticleFields on Article {
-        sys {
-            id
-        }
-        title
-        slug
-        content
-        category
-        date
-        thumbnail {
-            url
-        }
+  fragment ArticleFields on Article {
+    sys {
+      id
     }
+    title
+    slug
+    content
+    category
+    date
+    thumbnail {
+      url(transform: { width: 1426, height: 500, format: WEBP, quality: 100 })
+    }
+  }
 `;
