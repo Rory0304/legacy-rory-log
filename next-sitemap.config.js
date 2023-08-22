43da6c0e9ad961a0cb2afe035aab3cb81e6cf11f
Rory <mitty0304@naver.com>
2023-08-22 15:50:42 +0900

    chore: fix main url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_RORY_DEV || "https://rory0304.github.io",
+  siteUrl: process.env.NEXT_PUBLIC_RORY_DEV || "https://rory-log.vercel.app/",
   generateRobotsTxt: true,
   exclude: ["/404"],
   sitemapBaseFileName: "sitemap/sitemap",
