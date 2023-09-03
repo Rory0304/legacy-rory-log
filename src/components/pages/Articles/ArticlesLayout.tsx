d0d731cc00529549bc2061fbe4005bcbcde4e46a
Rory <mitty0304@naver.com>
2023-09-03 17:48:05 +0900

    fix(ArticlesLayout): fix pagination

--- src/components/pages/Articles/ArticlesLayout.tsx
+++ src/components/pages/Articles/ArticlesLayout.tsx
@@ -9,6 +9,7 @@ import { useUpdateEffect, useDebounce } from "react-use";
 
 import { ArticleList, ArticleTabs, ArticleSearchInput } from ".";
 import { ArticleType } from "src/app/api/articles/getArticles";
+import { isBrowser } from "src/utils/browser";
 
 //
 //
@@ -49,31 +50,32 @@ const ArticlesLayout: React.FC<ArticlesLayoutProps> = ({
     React.useState<ArticleType[]>(articleList);
 
   const total = Math.ceil(searchedArticleList.length / ARTICLES_PER_PAGE);
+  const offset = (page - 1) * ARTICLES_PER_PAGE;
 
   //
   //
   //
   useDebounce(() => setDebouncedSearchInput(searchInput), 300, [searchInput]);
 
-  // Reset pagination
-  React.useEffect(() => {
-    setPage(1);
-  }, [selectedTab, searchInput]);
-
   useUpdateEffect(() => {
     setSelectedTab("All");
     router.push(pathname + "?q=" + debouncedSearchInput);
   }, [debouncedSearchInput]);
 
+  //
   // Update searchedArticleList when debounced searchInput changes
+  //
   useUpdateEffect(() => {
     const searchedArticleList = articleList.filter((article) =>
       article.title?.includes(searchInputQueryParams ?? "")
     );
     setSearchedArticleList(searchedArticleList);
+    setPage(1);
   }, [searchInputQueryParams]);
 
+  //
   // Update searchedArticleList when searchTabQueryParams changes
+  //
   useUpdateEffect(() => {
     if (searchTabQueryParams) {
       const filteredArticleList =
@@ -81,13 +83,15 @@ const ArticlesLayout: React.FC<ArticlesLayoutProps> = ({
           ? articleList
           : articleList.filter((article) => article.category === selectedTab);
       setSearchedArticleList(filteredArticleList);
+      setPage(1);
     }
   }, [searchTabQueryParams]);
 
-  const handleTabChange = (tab: string) => {
-    setSelectedTab(tab);
-    router.push(pathname + "?tab=" + tab);
-  };
+  useUpdateEffect(() => {
+    if (isBrowser) {
+      window.scrollTo(0, 0);
+    }
+  }, [page]);
 
   return (
     <>
@@ -103,7 +107,7 @@ const ArticlesLayout: React.FC<ArticlesLayoutProps> = ({
       </Box>
 
       <Box display="flex" flexDirection={{ xs: "column", md: "row" }}>
-        <Box sx={{ flex: "0 0 auto", paddingRight: {xs: 0, md: 6 }}}>
+        <Box sx={{ flex: "0 0 auto", paddingRight: { xs: 0, md: 6 } }}>
           <Typography variant="h6" fontWeight={700} sx={{ paddingBottom: 2 }}>
             <Typography component="span" sx={{ marginRight: 1 }}>
               🏷️
@@ -114,7 +118,10 @@ const ArticlesLayout: React.FC<ArticlesLayoutProps> = ({
             tabs={tabList}
             tab={selectedTab}
             total={articleList.length}
-            onChange={handleTabChange}
+            onChange={(tab: string) => {
+              setSelectedTab(tab);
+              router.push(pathname + "?tab=" + tab);
+            }}
           />
         </Box>
         <Box>
@@ -123,7 +130,12 @@ const ArticlesLayout: React.FC<ArticlesLayoutProps> = ({
               총 {searchedArticleList.length}개의 게시글이 검색되었습니다.
             </Typography>
           ) : null}
-          <ArticleList articleList={searchedArticleList} />
+          <ArticleList
+            articleList={searchedArticleList.slice(
+              offset,
+              offset + ARTICLES_PER_PAGE
+            )}
+          />
           <Box>
             <Pagination
               shape="rounded"
