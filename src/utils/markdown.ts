e02a2911130d66bb335a4bae54392355a9c533dc
Rory <mitty0304@naver.com>
2023-10-30 15:33:16 +0900

    feat(markdown): convert heading id

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,14 +1,39 @@
 import DOMPurify from "isomorphic-dompurify";
-import { Marked } from "@ts-stack/markdown";
+import { Marked, Renderer } from "@ts-stack/markdown";
 import hljs from "highlight.js";
 
 const STRIPPING_HTML_TAG_REGEX_PATTERN = /(<([^>]+)>)/gi;
+
+/**
+ * Initialize markdown renderer
+ * ref: https://github.com/ts-stack/markdown
+ */
+class CustomRenderer extends Renderer {
+  override heading(text: string, level: number, raw: string) {
+    const regexp = /\s*{([^}]+)}$/;
+    const execArr = regexp.exec(text);
+    let id: string;
+
+    if (execArr) {
+      text = text.replace(regexp, "");
+      id = execArr[1];
+    } else {
+      id = text
+        .replace(/[^\w\sㄱ-ㅎ가-힣]/g, "")
+        .replace(/\s+/g, "-")
+        .toLocaleLowerCase();
+    }
+
+    return `<h${level} id="${id}">${text}</h${level}>`;
+  }
+}
 
 /**
  * Initialize markdown option
  * ref: https://www.npmjs.com/package/marked-highlight
  */
 Marked.setOptions({
+  renderer: new CustomRenderer(),
   langPrefix: "hljs language-",
   highlight: (code, lang) => {
     const language = lang || "plaintext";
